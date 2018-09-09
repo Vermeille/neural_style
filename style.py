7fc5473d8a5ca31cb8b12531eb926e851fdc423e
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-09-09 23:02:03 +0200

    init

---
+++ style.py
@@ -0,0 +1,160 @@
+import sys
+import numpy as np
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import gc
+from torchvision import models as M
+from torchvision import transforms
+import functools
+from PIL import Image
+import requests
+import torch.optim as O
+import io
+import argparse
+
+
+def gram(m):
+    b, c, h, w = m.shape
+    m = m.view(b, c, h * w)
+    m1 = m
+    m2 = m.permute(0, 2, 1)
+    g = torch.bmm(m1, m2) / (c * h * w)
+    return g
+
+
+def total_variation(input):
+    diff_h = ((input[:, :, 1:, :] - input[:, :, :-1, :]) ** 2).mean()
+    diff_v = ((input[:, :, :, 1:] - input[:, :, :, :-1]) ** 2).mean()
+    return torch.sqrt(diff_h + diff_v)
+
+
+def normalize(input):
+    norm_mean = torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).cuda()
+    norm_std = torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).cuda()
+    return (input - norm_mean) / norm_std
+
+
+class WithSavedActivations:
+    def __init__(self, model):
+        self.model = model
+        self.activations = {}
+        self.detach = True
+
+        # We want to save activations of convs and relus. Also, MaxPool creates
+        # actifacts so we replace them with AvgPool that makes things a bit
+        # cleaner.
+
+        for name, layer in self.model.named_children():
+            if isinstance(layer, nn.Conv2d):
+                layer.register_forward_hook(functools.partial(self._save, name))
+            if isinstance(layer, nn.ReLU):
+                self.model[int(name)] = nn.ReLU(inplace=False)
+            if isinstance(layer, nn.MaxPool2d):
+                self.model[int(name)] = nn.AvgPool2d(kernel_size=2, stride=2)
+
+
+    def _save(self, name, module, input, output):
+        if self.detach:
+            self.activations[name] = output.detach().clone()
+        else:
+            self.activations[name] = output.clone()
+
+    def __call__(self, input):
+        self.activations = {}
+        self.model(input)
+        return self.activations
+
+
+def lowres_noise_like(tens):
+    lowres_size = (
+            tens.shape[0],
+            tens.shape[1] // 8,
+            tens.shape[2] // 8)
+
+    canvas = torch.randn(*lowres_size) / 50 + 0.5
+    canvas = F.interpolate(
+            canvas[None],
+            size=tens.shape[1:],
+            mode='bilinear',
+            align_corners=False)[0].detach().cuda()
+    canvas.requires_grad = True
+    return canvas
+
+
+def npimg_to_tensor(np_img):
+    return torch.FloatTensor(np_img / 255).permute(2, 0, 1).cuda()
+
+
+def artistic_style(content_img, style_img, m=None, style_ratio=1e1):
+    content_layers = ['21']
+    if m is None:
+        m = M.vgg19(pretrained=True).cuda().eval()
+        m = WithSavedActivations(m.features)
+
+    m.detach = True
+    torch_img = npimg_to_tensor(content_img)
+    photo_activations = m(normalize(torch_img[None]))
+
+    torch_style = npimg_to_tensor(style_img)
+    style_activations = m(normalize(torch_style[None]))
+
+    canvas = torch.randn_like(torch_img, requires_grad=True)
+
+    grams = {layer_id: gram(layer_data).detach()
+             for layer_id, layer_data in style_activations.items()}
+
+    canvas = lowres_noise_like(torch_img)
+    del torch_img
+    del torch_style
+    del style_activations
+
+    photo_activations = {
+            i: p
+            for i, p in photo_activations.items()
+            if i in content_layers}
+
+    opt = O.LBFGS([canvas], lr=0.5, history_size=1)
+
+    for i in range(30):
+        def make_loss():
+            gc.collect()
+            opt.zero_grad()
+            m.detach = False
+            input_img = torch.sigmoid(canvas[None])
+            activations = m(normalize(input_img))
+            style_loss = 0
+            for j in ['0', '5', '10', '19', '28']:
+                style_loss += 0.2 * F.mse_loss(
+                        gram(activations[j]), grams[j], reduction='sum')
+
+            content_loss = 0
+            for j in content_layers:
+                content_loss += F.mse_loss(activations[j], photo_activations[j])
+
+            loss = content_loss + style_ratio * style_loss
+            loss.backward()
+            return loss
+
+        loss = opt.step(make_loss)
+        if i % 10 == 0:
+            print(i, loss.item())
+
+    return (255 * torch.sigmoid(canvas)
+            .cpu().detach().numpy().transpose(1, 2, 0)).astype('uint8')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description="Implementation of Neural Artistic Style by Gatys")
+    parser.add_argument('--content', required=True)
+    parser.add_argument('--style', required=True)
+    parser.add_argument('--out', required=True)
+    parser.add_argument('--content_scale', type=float)
+    parser.add_argument('--style_scale', type=float)
+    args = parser.parse_args(sys.argv[1:])
+
+    result = style_from_uris(args)
+    result = Image.fromarray(result)
+    result.save(args.out)
+
---
+++ style_server.py
@@ -0,0 +1,100 @@
+from flask import Flask, request
+import requests
+import style
+from torchvision import models as M
+import base64
+import io
+from PIL import Image
+import numpy as np
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/tmp/'
+
+m = M.vgg19(pretrained=True).cuda().eval()
+m = style.WithSavedActivations(m.features)
+
+
+def wget(url, scale=None):
+    r = requests.get(url)
+    print(r.status_code)
+    with io.BytesIO(r.content) as f:
+        return open_img(f, scale)
+
+
+def open_img(path, size=None):
+    img = Image.open(path)
+    if size is not None:
+        img.thumbnail((size, size))
+    return np.array(img)
+
+
+def img_to_data_url(img):
+    with io.BytesIO() as f:
+        img = Image.fromarray(img, 'RGB')
+        img.save(f, 'jpeg')
+        f.seek(0)
+        return 'data:image/jpg;base64,' + base64.b64encode(f.read()).decode()
+
+
+@app.route('/go', methods=['GET', 'POST'])
+def go():
+    args = request.args
+
+    if args.get('preview', 'off') == 'on':
+        content_scale = 128
+        style_scale = 128
+    else:
+        content_scale = 512
+        style_scale = 512
+
+    if args['content'].startswith('http'):
+        content = wget(args['content'], content_scale)
+    else:
+        content = open_img(args['content'], content_scale)
+
+    if args['style'].startswith('http'):
+        style_img = wget(args['style'], style_scale)
+    else:
+        style_img = open_img(args['style'], style_scale)
+
+    print(args)
+    result = style.artistic_style(content, style_img, m,
+            float(args.get('ratio', '1e1')))
+    return '<img src="' + img_to_data_url(result) +'" />'
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return """
+<!doctype html>
+<html>
+    <body>
+        <h1>Neural Style</h1>
+        <form id="upload" action="/go" method="GET">
+            <input
+                type="text"
+                name="content"
+                placeholder="content URL"
+                required/>
+            <input
+                type="text"
+                name="style"
+                placeholder="style URL"
+                required/>
+            <input
+                type="number"
+                name="ratio"
+                placeholder="Loss ratio" />
+            <input type="checkbox" name="preview" checked/>
+            <input type="submit"/>
+        </form>
+    </body>
+    <script>
+window.upload.onsubmit = (e) => {
+};
+    </script>
+</html>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)

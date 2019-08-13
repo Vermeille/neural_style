8e4097426a75b6a7f1f462dcda35a5ea5f3b50a2
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-08-12 23:36:50 -0400

    now based on torchelies recipe

--- style.py
+++ style.py
@@ -5,53 +5,9 @@ from torchvision import transforms
 from PIL import Image
 import torch.optim as O
 import argparse
-from torchelie.loss import NeuralStyleLoss
+from torchelie.recipes.neural_style import NeuralStyleRecipe
 from colors import transfer_colors
-from image import get_parameterized_img
 import torch
-
-
-def npimg_to_tensor(np_img):
-    return transforms.ToTensor()(np_img)
-
-
-class ArtisticStyleOptimizer:
-    def __init__(self, device="cpu"):
-        self.loss = NeuralStyleLoss().to(device)
-        self.device = device
-
-
-    def build_ref_acts(self, content_img, style_img, style_ratio, content_layers):
-        self.loss.set_style(npimg_to_tensor(style_img).to(self.device), style_ratio)
-        self.loss.set_content(
-                npimg_to_tensor(content_img).to(self.device), content_layers)
-
-
-    def __call__(self, content_img, style_img, style_ratio, content_layers=None):
-        self.build_ref_acts(content_img, style_img, style_ratio, content_layers)
-        canvas = get_parameterized_img(
-                3, content_img.height, content_img.width, backend='limpid').to(self.device)
-        return self.optimize_img(canvas)
-
-
-    def optimize_img(self, canvas):
-        opt = O.LBFGS(canvas.parameters(), lr=0.2, history_size=20)
-
-        prev_loss = None
-        for i in range(100):
-            def make_loss():
-                opt.zero_grad()
-                input_img = canvas()
-                loss = self.loss(input_img)
-                loss.backward()
-                return loss
-
-            loss = opt.step(make_loss).item()
-            if prev_loss is not None and loss > prev_loss * 0.95:
-                break
-            prev_loss = loss
-                
-        return transforms.ToPILImage()(canvas().cpu().detach()[0])
 
 
 def go(args, stylizer):
@@ -89,7 +45,7 @@ if __name__ == '__main__':
     parser.add_argument('--interactive', action='store_true')
     args = parser.parse_args(sys.argv[1:])
 
-    stylizer = ArtisticStyleOptimizer(device=args.device)
+    stylizer = NeuralStyleRecipe(device=args.device)
     go(args, stylizer)
     if args.interactive:
         while True:
